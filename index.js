52350c5f2184c57142bd4d4ad6a0fd326fa6f5d7
Abady <gamersspeaks@gmail.com>
2018-11-18 19:37:24 +0300

    make it better

---
+++ index.js
@@ -0,0 +1,57 @@
+const ytdl = require('ytdl-core')
+const YouTube = require('simple-youtube-api')
+
+class Player { 
+    /**
+     * @param {EventListenerObject} client - Your d.js client. 
+     * @param {String} ytkey - Your Youtube v3 data key. 
+     * @param {String} channel - The channel you want bot to stay in id.
+     * @param {String} playlist - playlist or video for the bot to play.
+     */
+constructor(client, ytkey , channel, playlist) {
+this.client = client || null
+this.ytkey = ytkey || null
+this.channel = channel || null
+this.playlist = playlist || null
+}
+
+async play() {
+const queue = []; 
+const youtube = new YouTube(this.ytkey); 
+const playlist = await youtube.getPlaylist(this.playlist);
+const getVideos = await playlist.getVideos(); 
+getVideos.forEach(video => {
+queue.push({
+    title: video.title, 
+    url: video.url
+})
+}) 
+
+this.client.on('ready', async () => {
+    stream(this.client, this.channel)
+})
+
+
+const client = this.client
+const channel = this.channel
+
+async function stream() {
+    const connection = await client.channels.get(channel).join(); 
+    const dispatcher = connection.playStream(ytdl(queue[0].url, {
+        filter: 'audioonly',
+        quality: 'highestaudio',
+        audioEncoding: "opus"
+    }))
+    client.user.setActivity(`${queue[0].title}`, {type: "LISTENING"});
+
+    dispatcher.on('end', () => {
+        const loop = queue.shift();
+        queue.push(loop);
+        return stream(client, channel, queue[0].url);
+    })
+     }}
+
+
+}
+
+module.exports = Player
---
+++ test.js
@@ -0,0 +1,5 @@
+const {Client} = require('discord.js');
+const client = new Client(); 
+const player = require('./index'); 
+const Player = new player(client, 'AIzaSyC47u9iM3B72Jx-trbZ27xDjEI560i4d44', '473645724568125451', 'https://www.youtube.com/watch?v=wXcdYBh3hgg&list=PLVuQBUGB87-gomoG36CV4wMZCkGPGKw3p'); 
+client.login('NDcyMTUyOTUyNzAzMDI1MTUy.DtMlkQ.WIKFOuiORKamoLNayCeacnVimvM'); 
