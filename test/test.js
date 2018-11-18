4e27ab149a900e916cff2d07835926987dfc4cf5
Abady <gamersspeaks@gmail.com>
2018-11-18 22:58:18 +0300

    Secure things...

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const {Client, RichEmbed} = require('discord.js');
 const client = new Client(); 
 const player = require('../index'); 
-const Player = new player(client, 'AIzaSyC47u9iM3B72Jx-trbZ27xDjEI560i4d44', '473645724568125451', 'https://www.youtube.com/watch?v=wXcdYBh3hgg&list=PLVuQBUGB87-gomoG36CV4wMZCkGPGKw3p'); 
-client.login('NDcyMTUyOTUyNzAzMDI1MTUy.DtMlkQ.WIKFOuiORKamoLNayCeacnVimvM'); 
+const Player = new player(client, process.env.YT_KEY, process.env.CHANNEL, process.env.PLAYLIST); 
+client.login(process.env.TOKEN); 
 Player.play();
 
 client.on('ready', () => {
