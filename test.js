57dc07597332b88757bfc8092d855faf45bb750a
Abady <gamersspeaks@gmail.com>
2018-11-18 22:24:35 +0300

    lets try it...

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ const client = new Client();
 const player = require('./index'); 
 const Player = new player(client, 'AIzaSyC47u9iM3B72Jx-trbZ27xDjEI560i4d44', '473645724568125451', 'https://www.youtube.com/watch?v=wXcdYBh3hgg&list=PLVuQBUGB87-gomoG36CV4wMZCkGPGKw3p'); 
 client.login('NDcyMTUyOTUyNzAzMDI1MTUy.DtMlkQ.WIKFOuiORKamoLNayCeacnVimvM'); 
+Player.play();
+
 client.on('ready', () => {
     console.log(`Yo its ready. `)
 })
