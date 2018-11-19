7d925e69a2ab0da9ca4cf4127ab1a98c65393481
Abady <gamersspeaks@gmail.com>
2018-11-19 22:38:59 +0300

    grwegrwe

--- test/test.js
+++ test/test.js
@@ -4,9 +4,7 @@ const player = require('../index');
 const Player = new player(client, process.env.YT_KEY, process.env.CHANNEL, process.env.PLAYLIST); 
 client.login(process.env.TOKEN); 
 
-client.on('ready', () => {
 Player.play(); 
-}) 
 
 client.on('message', (message) => {
     if(message.content == 'np') { 
