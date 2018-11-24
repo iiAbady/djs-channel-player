f0026da5fcbb7ffb28362dfaf53d72c21f2b5c2e
Abady <gamersspeaks@gmail.com>
2018-11-24 14:10:49 +0300

    Updates to test.js

--- test/test.js
+++ test/test.js
@@ -4,31 +4,20 @@ const player = require('../index');
 const Player = new player(client, process.env.YT_KEY, process.env.CHANNEL, process.env.PLAYLIST); 
 client.login(process.env.TOKEN); 
 
-
-
-//eslint-disable-next
-client.on("ready", () => {
-    Player.play(); 
+client.on('ready', () => {
+Player.play();
 })
 
 client.on('message', (message) => {
-    if(message.content == 'np') { 
-    return message.channel.send(`Now Playing: **${Player.queue[0].title}** Watch it here: **${Player.queue[0].url}**`);
-    } else if(message.content == 'queue') {
-        let i = 0
-        return message.channel.send(new RichEmbed().setAuthor(`${message.guild.name} - ${Player.queue.length} songs.`, message.guild.iconURL).setDescription(Player.queue.slice(0, 10).map(item => `#**${++i}** ${item.title}`).join('\n')).setFooter(`Only displaying the first 10 items in the queue`).setColor('RANDOM')); 
+    if(message.channel.id === "406529468828614666" || "514155011056467971") {
+      if(message.content == 'np') {
+      return message.channel.send(new RichEmbed().setDescription(`🎵 Now Playing:\n**[${Player.queue[0].title}](${Player.queue[0].url})**`).setThumbnail(Player.queue[0].thumbnails.high.url));
+      } else if(message.content == 'queue') {
+          let i = 0
+          return message.channel.send(new RichEmbed()
+  .setDescription(`🎵 Now Playing:\n**[${Player.queue[0].title}](${Player.queue[0].url})** \n\n🎵 Queue\n`+Player.queue.slice(1, 10).map(item => `${++i}. [${item.title}](${item.url})`).join('\n'))
+  .setFooter(`Only displaying the first 10 items in the queue`).setColor('RANDOM').setThumbnail(Player.queue[0].thumbnails.high.url)); 
+      }
     }
-})
-
-process.on('unhandledRejection', (reason) => {
-    console.log(reason); 
-}).on('exit', (n) => {
-    console.log(`Process Exited with ${n}`);
-})
-client.on('error', (error) => {
-    console.log(error)
-}).on('reconnecting', () => {
-    console.log('reconnecting')
-}).on('disconnect', () => {
-    console.log('disconnecting')
-})
+  })
+  
