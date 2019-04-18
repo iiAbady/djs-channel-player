0d0992ad0402eb15c654c56bc608c222052efb99
Abady <gamersspeaks@gmail.com>
2019-04-18 03:31:25 +0300

    chore(deps): use ytdl-core-discord & eslint-config-aqua

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
-const ytdl = require('ytdl-core')
-const YouTube = require('simple-youtube-api')
-const version = require('./package.json').version
+const ytdl = require('ytdl-core-discord');
+const YouTube = require('simple-youtube-api');
+const { version } = require('./package.json');
 
-class Player { 
-    /**
-     * @param {Client} client - Your d.js client. 
-     * @param {String} ytkey - Your Youtube v3 data key. 
-     * @param {String} channel - The channel you want bot to stay in id.
-     * @param {String} playlist - playlist or video for the bot to play.
+class Player {
+	/**
+     * @param {Client} client - Your d.js client.
+     * @param {string} ytkey - Your Youtube v3 data key.
+     * @param {string} channel - The channel you want bot to stay in id.
+     * @param {string} playlist - playlist or video for the bot to play.
      */
-constructor(client, ytkey , channel, playlist) {
-this.client = client || null
-this.ytkey = ytkey || null
-this.channel = channel || null
-this.playlist = playlist || null
-this.queue = null; 
-this.dispatcher = null; 
+	constructor(client, ytkey, channel, playlist) {
+		this.client = client;
+		this.ytkey = ytkey;
+		this.channel = channel;
+		this.playlist = playlist;
+		this.queue = null;
+		this.dispatcher = null;
+	}
+
+	async play() {
+		const youtube = new YouTube(this.ytkey);
+		const playlist = await youtube.getPlaylist(this.playlist);
+		const getVideos = await playlist.getVideos();
+		const queue = getVideos.filter(v => v.thumbnails !== undefined);
+		this.queue = queue;
+
+		const stream = (this, async () => {
+			try {
+				// eslint-disable-next-line no-var
+				var connection = await this.client.channels.get(this.channel).join();
+			} catch (error) {
+				console.error(`[ERROR:CONNECTION] Error occurred when joining voice channel.`);
+			}
+			const ytStream = await ytdl(queue[0].url).on('error', err => {
+				console.error(`[ERROR:STREAMING] Couldn't play **${queue[0].title}**`, err);
+				queue.shift();
+			});
+			// eslint-disable-next-line block-scoped-var
+			const dispatcher = connection.playOpusStream(ytStream)
+				.on('end', () => {
+					const loop = queue.shift();
+					queue.push(loop);
+					stream(this.client, this.channel).then(dispatcher => this.dispatcher = dispatcher).catch(err => console.log(`[ERROR:STREAMING] ${err}`));
+				}).on('error', err => {
+					console.error(`[ERROR:DISPATCHER] ${err}`);
+				})
+				.on('start', () => {
+					// eslint-disable-next-line block-scoped-var
+					connection.player.streamingData.pausedTime = 0;
+					this.client.user.setActivity(`${queue[0].title}`, { type: 'LISTENING' });
+					console.log(`[INFO] Started streaming: ${queue[0].title} at ${this.client.channels.get(this.channel).name}.`);
+				});
+			return dispatcher;
+		});
+
+		this.client.user.setActivity('Loading...', { type: 'LISTENING' });
+		stream(this.client, this.channel).then(dispatcher => this.dispatcher = dispatcher).catch(err => console.error(`[ERROR:STREAMING] ${err}`));
+	}
 }
 
-async play() {
-const youtube = new YouTube(this.ytkey); 
-const playlist = await youtube.getPlaylist(this.playlist);
-const getVideos = await playlist.getVideos(); 
-const queue = getVideos.filter(v => v.thumbnails !== undefined);  
-this.queue = queue;
-
-const stream = (this, async () => {
-    try {
-        var connection = await this.client.channels.get(this.channel).join();    
-    } catch (error) {
-        console.error(`[ERROR:CONNECTION] Error occurred when joining voice channel.`)
-    }
-    const ytStream = ytdl(queue[0].url, {
-        filter: "audioonly"
-    }).on("error", err => {
-        console.error(`[ERROR:STREAMING] Couldn't play **${queue[0].title}**`, err)
-        queue.shift(); 
-    })
-    const dispatcher = connection.playStream(ytStream)
-    .on('end', () => {
-        const loop = queue.shift();
-        queue.push(loop);
-        stream(this.client, this.channel).then(dispatcher => this.dispatcher = dispatcher).catch(err => console.log(`[ERROR:STREAMING] ${err}`));
-    }).on('error', (err) => {
-        console.error(`[ERROR:DISPATCHER] ${err}`);
-    }).on('start', () => {
-        connection.player.streamingData.pausedTime = 0; 
-        this.client.user.setActivity(`${queue[0].title}`, {type: "LISTENING"});
-        console.log(`[INFO] Started streaming: ${queue[0].title} at ${this.client.channels.get(this.channel).name}.`)
-    }); 
-    return dispatcher;
-})
-
-this.client.user.setActivity("Loading...", {type: "LISTENING"}) 
-stream(this.client, this.channel).then(dispatcher => this.dispatcher = dispatcher).catch(err => console.error(`[ERROR:STREAMING] ${err}`)); 
-
-     }    
-    }
-
-module.exports = Player
-exports.version = version
+module.exports = Player;
+exports.version = version;
--- test/test.js
+++ test/test.js
@@ -1,29 +1,33 @@
-const {Client, RichEmbed} = require('discord.js');
-const client = new Client(); 
+const { Client, RichEmbed } = require('discord.js');
+const client = new Client();
 const player = require('../index');
-const Player = new player(client, process.env.YT_KEY, process.env.CHANNEL, process.env.PLAYLIST); 
-client.login(process.env.TOKEN); 
-const staff = ['356355452407119873', '171259176029257728']
+const Player = new player(client, process.env.YT_KEY, process.env.CHANNEL, process.env.PLAYLIST);
+client.login(process.env.TOKEN);
+const staff = ['356355452407119873', '171259176029257728'];
 
 client.on('ready', () => {
-Player.play();
-})
+	Player.play();
+});
 
-client.on('error', (error) => console.error(error)); 
+client.on('error', error => console.error(error));
 
-client.on('message', (message) => {
-    const prefixMention = new RegExp(`^<@!?${client.user.id}> `),prefix=message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : 'os!';
-    if(message.channel.id === "406529468828614666" || "514155011056467971") {
-      if(message.content == `${prefix}np`) {
-      return message.channel.send(new RichEmbed().setDescription(`🎵 Now Playing:\n**[${Player.queue[0].title}](${Player.queue[0].url})**`).setThumbnail(Player.queue[0].thumbnails.high.url).setColor(`BLUE`));
-      } else if(message.content == `${prefix}queue`) {
-          let i = 0
-          return message.channel.send(new RichEmbed()
-  .setDescription(`🎵 Now Playing:\n**[${Player.queue[0].title}](${Player.queue[0].url})** \n\n🎵 Queue\n`+Player.queue.slice(1, 10).map(item => `${++i}. [${item.title}](${item.url})`).join('\n'))
-  .setFooter(`Only displaying the first 10 items in the queue`).setColor('RANDOM').setThumbnail(Player.queue[0].thumbnails.high.url)); 
-    } else if(staff.includes(message.author.id) && message.content == `${prefix}skip`) {
-      message.channel.send(`تخطي الاغنية:\n**${Player.queue[0].title}**`)
-      return Player.dispatcher.end(); 
-    }
-    }
-  })
+client.on('message', message => {
+	const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+	const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : 'os!';
+	if (message.channel.id === '406529468828614666' || '514155011056467971') {
+		if (message.content === `${prefix}np`) {
+			return message.channel.send(new RichEmbed().setDescription(`🎵 Now Playing:\n**[${Player.queue[0].title}](${Player.queue[0].url})**`).setThumbnail(Player.queue[0].thumbnails.high.url)
+				.setColor(`BLUE`));
+		} else if (message.content === `${prefix}queue`) {
+			let i = 0;
+			return message.channel.send(new RichEmbed()
+				.setDescription(`🎵 Now Playing:\n**[${Player.queue[0].title}](${Player.queue[0].url})** \n\n🎵 Queue\n${Player.queue.slice(1, 10).map(item => `${++i}. [${item.title}](${item.url})`).join('\n')}`)
+				.setFooter(`Only displaying the first 10 items in the queue`)
+				.setColor('RANDOM')
+				.setThumbnail(Player.queue[0].thumbnails.high.url));
+		} else if (staff.includes(message.author.id) && message.content === `${prefix}skip`) {
+			message.channel.send(`تخطي الاغنية:\n**${Player.queue[0].title}**`);
+			return Player.dispatcher.end();
+		}
+	}
+});
